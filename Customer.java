5cb8a745652a575f7ef8ed4e56224ab765f97bdb
Chirag <76880977+chirag127@users.noreply.github.com>
2023-04-17 14:34:02 +0000

    init

---
+++ Customer.java
@@ -0,0 +1,93 @@
+public class Customer {
+    private String name;
+    private int age;
+    private String city;
+    private String loanType;
+    private double amount;
+    private String mobile;
+    private String email;
+    private String dateOfBirth;
+    private double annualIncome;
+    private double liability;
+
+    // Getters and setters for all attributes
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    void setAge(int age) {
+        this.age = age;
+    }
+
+    String getCity() {
+        return city;
+    }
+
+    void setCity(String city) {
+        this.city = city;
+    }
+
+    String getLoanType() {
+        return loanType;
+    }
+
+    void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+
+    double getAmount() {
+        return amount;
+    }
+
+    void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    String getMobile() {
+        return mobile;
+    }
+
+    void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    String getEmail() {
+        return email;
+    }
+
+    void setEmail(String email) {
+        this.email = email;
+    }
+
+    String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    double getAnnualIncome() {
+        return annualIncome;
+    }
+
+    void setAnnualIncome(double annualIncome) {
+        this.annualIncome = annualIncome;
+    }
+
+    double getLiability() {
+        return liability;
+    }
+
+    void setLiability(double liability) {
+        this.liability = liability;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create instances of necessary classes
+        Customer customer = new Customer();
+        Loan loan = new Loan();
+        LoanApplication loanApplication = new LoanApplication(customer, loan);
+        DefaulterDatabase defaulterDatabase = new DefaulterDatabase();
+
+        // Perform necessary actions, such as applying for a loan, quick data entry, etc.
+        // Example:
+        loanApplication.applyForLoan();
+        loanApplication.quickDataEntry();
+        boolean isDefaulter = loanApplication.checkDefaulter();
+        if (!isDefaulter) {
+            loanApplication.decideLoanApplication();
+        } else {
+            // Handle defaulter case
+        }
+    }
+}
